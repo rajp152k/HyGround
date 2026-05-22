3232b043330580b8f31560fb274be0aed775652a
Raj <rajp152k@gmail.com>
2026-05-22 15:23:37 +0530

    Use position-aware semantic token resolution

--- tests/test_semantic.py
+++ tests/test_semantic.py
@@ -9,7 +9,7 @@ def test_semantic_tokens_classify_hy_source() -> None:
     index = WorkspaceIndex()
     index.update_document(URI, '(defreader bang [] 1)\n(defn foo [x]\n  "Docs"\n  (+ x 1))\n')
 
-    tokens = semantic_tokens(source, lambda name: index.resolve(URI, name))
+    tokens = semantic_tokens(source, lambda name, line, character: index.resolve(URI, name, line, character))
     by_text = {(source.splitlines()[token.line][token.start : token.start + token.length], token.token_type) for token in tokens}
 
     assert ("defn", "keyword") in by_text
@@ -22,6 +22,21 @@ def test_semantic_tokens_classify_hy_source() -> None:
 
 
 def test_semantic_token_encoding_is_relative() -> None:
-    data = encode_semantic_tokens(semantic_tokens("1\n  2\n", lambda name: None))
+    data = encode_semantic_tokens(semantic_tokens("1\n  2\n", lambda name, line, character: None))
 
     assert data == [0, 0, 1, 7, 0, 1, 2, 1, 7, 0]
+
+
+def test_semantic_tokens_resolve_parameters_with_position() -> None:
+    source = "(defn foo [print]\n  (print 1))\n(print \"outside\")\n"
+    index = WorkspaceIndex()
+    index.update_document(URI, source)
+
+    tokens = semantic_tokens(source, lambda name, line, character: index.resolve(URI, name, line, character))
+    print_tokens = [
+        token
+        for token in tokens
+        if source.splitlines()[token.line][token.start : token.start + token.length] == "print"
+    ]
+
+    assert [token.token_type for token in print_tokens] == ["variable", "variable", "function"]
